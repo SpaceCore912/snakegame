fed4c4411dd41e0d0acaa53fd2919e842740b9c5
SpaceCore912 <tykhonbyshkin07@gmail.com>
2024-09-23 16:25:32 -0400

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -11,9 +11,8 @@ def print_field():
 	for cell in CELLS:
 		if cell in snake_body: 
 			print(Fore.GREEN + 'S', end='')# check that a tuple is inside a list
-		#if cell[0]==0 or cell[1]==(field_height-1) or cell[0]==field_width-1 or cell[1]== 0:
-			#print('#')
-		elif cell[1] in (0, field_height-1) or cell[0] in (0, field_width-1) : #checks that a number is inside/between two numbers
+		
+		elif cell[1] in (0, field_height-1) or cell[0] in (0, field_width-1) : #checks that a number is between two numbers
 			if cell[0]==field_width-1:
 					print(Fore.CYAN +'#')			
 			else: print(Fore.CYAN + '#', end = '')
@@ -25,9 +24,7 @@ def print_field():
 def update_snake():
 	global eaten
 	#head, middle, tail=snake_body
-	#snake_body[0] = (head[0]+direction[0], head[1]+ direction[1])
-	#snake_body[1]=head
-	#snake_body[2]=middle
+	
 	new_head =snake_body[0][0]+direction[0], snake_body[0][1]+ direction[1]
 	snake_body.insert(0, new_head)
 	if not eaten:
@@ -58,14 +55,10 @@ def place_apple():
 		
 	
 #settings
-field_width=32#should be uppercase
-field_height=16#should be uppercase
+field_width=32
+field_height=16
 CELLS=[(col , row) for row in range(field_height) for col in range(field_width)]
-#cells=[]
-#for row in range( field_height):
-	#for col in range(field_width):
-		#cells.append((col,row))
-		
+
 if platform == "win32" or platform == "cygwin":
 	clearcmd = "cls"
 else:
@@ -104,16 +97,6 @@ while True:
 			break
 
 
-	#commands=input()
-	#if txt=='w':
-		#direction =DIRECTIONS['up']
-	#elif txt=='s':
-		#direction =DIRECTIONS['down']
-	#elif txt=='d':
-		#direction =DIRECTIONS['right']
-	#elif txt=='a':
-		#direction =DIRECTIONS['left']
-	
 	#update the game
 	update_snake()
 	apple_collision()
